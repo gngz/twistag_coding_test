88c665720722ac4f2422b8be741c25aae85c3c18
Gonçalo Passos <dev@diogopassos.pt>
2023-11-24 16:33:35 +0000

    Updare repository model

--- src/models/repository.ts
+++ src/models/repository.ts
@@ -2,5 +2,6 @@ import { APIRepositoryModel } from '@/services/github/models/repository';
 
 export type RepositoryModel = APIRepositoryModel & {
   color: string;
+  stats?: number[];
   selected: boolean;
 };
