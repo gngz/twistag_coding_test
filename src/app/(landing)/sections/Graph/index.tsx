b482a2aaf8904e1508e21482b7feee01757e497c
Gonçalo Passos <dev@diogopassos.pt>
2023-11-24 16:40:47 +0000

    Update Graph

--- src/app/(landing)/sections/Graph/index.tsx
+++ src/app/(landing)/sections/Graph/index.tsx
@@ -1,11 +1,70 @@
 'use client';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts';
 import { RepoContext } from '@/providers/RepoContext';
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
+import { RepositoryModel } from '@/models/repository';
+import CustomTooltip from '@/components/CustomTooltip';
 
 type Props = {};
 
+function parseData(data: RepositoryModel[]) {
+  const parsedData = new Array<Record<string, number | string>>();
+  for (let week = 0; week < 52; week++) {
+    const weekEntry: Record<string, number | string> = {};
+    weekEntry['week'] = week;
+    for (const repo of data) {
+      weekEntry[repo.full_name] = repo.stats ? repo.stats[week] : 0;
+    }
+    parsedData.push(weekEntry);
+  }
+  return parsedData;
+}
+
 export default function Graph({}: Props) {
   const context = useContext(RepoContext);
+  const parsedData = useMemo(() => {
+    return parseData(context);
+  }, [context]);
 
-  return <div>{JSON.stringify(context)}</div>;
+  return (
+    <ResponsiveContainer className='p-10'>
+      <LineChart data={parsedData}>
+        {context.map((repo) => (
+          <Line
+            key={repo.id}
+            type='monotone'
+            dataKey={repo.full_name}
+            stroke={repo.color}
+            strokeOpacity={0.3}
+            dot={{ strokeWidth: 2, stroke: repo.color, fill: 'white', r: 6 }}
+            activeDot={{ fill: repo.color, strokeWidth: 0, r: 6 }}
+            strokeWidth={3}
+          />
+        ))}
+        <Tooltip content={<CustomTooltip />} />
+        <XAxis
+          strokeWidth={3}
+          stroke='#37374A'
+          tick={<></>}
+          tickLine={true}
+          mirror
+          dataKey='week'
+        />
+        <YAxis
+          strokeWidth={3}
+          stroke='#37374A'
+          tick={<></>}
+          tickLine={true}
+          mirror
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 }
