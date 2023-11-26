2e3ba9c7fe020d30801e2f443b90782f24634e00
Gonçalo Passos <dev@diogopassos.pt>
2023-11-26 15:18:50 +0000

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
+/**
+ * Checks if the current environment matches the given environment.
+ *
+ * @param {('production' | 'development')} env - The environment to check against.
+ * @return {boolean} Returns true if the current environment matches the given environment, false otherwise.
+ */
+function isEnvironment(env) {
+  return process.env.NODE_ENV === env;
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  poweredByHeader: false,
+  compress: process.env.COMPRESS ?? true,
+  output: isEnvironment('production') ? 'standalone' : 'export',
+};
 
 module.exports = nextConfig;
